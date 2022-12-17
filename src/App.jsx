0a3345daaabbe5f276041402dc5b5ac12a64e19a
unknown <luis.deharoparames@emeal.nttdata.com>
2022-12-17 18:20:03 +0100

    App Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useReactTable } from "@tanstack/react-table";
+import React, { useState } from "react";
 
 function App() {
-  //App Calendar
+  //const table = useReactTable(options);
+  const [month, setMonth ] = useState();
+
+  return (
+    <form id="myForm">
+      <div id="divFormMonth">
+        <label for="month"> Introduce el mes: </label>
+        <input type="text" id="month" name="month" placeholder="enero..." />
+      </div>
+
+      <div id="divFormYear">
+        <label for="year"> Introduce el año: </label>
+        <input type="text" id="year" name="year" placeholder="2000" />
+      </div>
+      <div id="divExplicacion">
+        <label for="">
+          *Si Introduces mes y año solo mostrara por pantalla el mes del año
+          seleccionado
+        </label>
+        <label for="">
+          *Si Introduces el año solo mostrara por pantalla los meses del año
+          seleccionado
+        </label>
+      </div>
+      <div id="divButton">
+        <button type="submit" id="buttonStyle">
+          Aceptar
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default App
+export default App;
