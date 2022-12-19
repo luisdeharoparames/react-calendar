94e04ba701b3ce59a25ec5831b9a73f34354eff5
unknown <luis.deharoparames@emeal.nttdata.com>
2022-12-19 11:19:04 +0100

    Array Months

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,62 @@
-import { useState } from "react";
 import "./App.css";
 import { useReactTable } from "@tanstack/react-table";
-import React, { useState } from "react";
+import { useState } from "react";
+import './MONTHS';
 
-function App() {
-  //const table = useReactTable(options);
-  const [month, setMonth ] = useState();
+const App = () => {
+  // console.log(MONTHS);
+  const handleSubmitMonth = (eMonth) => {
+    if(eMonth){
+
+    }
+  };
+
+  const handleSubmitYear = (eYear) => {
+    console.log("Year: " + eYear.target.value);
+  };
+
 
   return (
-    <form id="myForm">
-      <div id="divFormMonth">
-        <label for="month"> Introduce el mes: </label>
-        <input type="text" id="month" name="month" placeholder="enero..." />
-      </div>
-
-      <div id="divFormYear">
-        <label for="year"> Introduce el año: </label>
-        <input type="text" id="year" name="year" placeholder="2000" />
-      </div>
-      <div id="divExplicacion">
-        <label for="">
-          *Si Introduces mes y año solo mostrara por pantalla el mes del año
-          seleccionado
-        </label>
-        <label for="">
-          *Si Introduces el año solo mostrara por pantalla los meses del año
-          seleccionado
-        </label>
-      </div>
-      <div id="divButton">
-        <button type="submit" id="buttonStyle">
-          Aceptar
-        </button>
-      </div>
-    </form>
+    <div className="App">
+      <form id="myForm">
+        <div id="divFormMonth">
+          <label> Introduce el mes: </label>
+          <input
+            type="text"
+            id="month"
+            name="month"
+            placeholder="enero..."
+            onChange={handleSubmitMonth}
+          ></input>
+        </div>
+        <div id="divFormYear">
+          <label> Introduce el año: </label>
+          <input
+            type="text"
+            id="year"
+            name="year"
+            placeholder="2000"
+            onChange={handleSubmitYear}
+          />
+        </div>
+        <div id="divExplicacion">
+          <label>
+            *Si Introduces mes y año solo mostrara por pantalla el mes del año
+            seleccionado
+          </label>
+          <label>
+            *Si Introduces el año solo mostrara por pantalla los meses del año
+            seleccionado
+          </label>
+        </div>
+        <div id="divButton">
+          <button type="submit" id="buttonStyle">
+            Aceptar
+          </button>
+        </div>
+      </form>
+    </div>
   );
-}
+};
 
 export default App;
