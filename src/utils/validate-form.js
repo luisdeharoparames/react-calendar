863aee3e0fa7bac24cd1e611bdbb63816614104e
luisdeharoparames <luis.deharoparames@emeal.nttdata.com>
2022-12-21 10:45:27 +0100

    Validate js File

--- src/utils/validate-form.js
+++ src/utils/validate-form.js
@@ -1,6 +1,7 @@
 import { MONTHS } from "../data/MONTHS";
-
+// import { createCalendar } from "./calendar";
 const monthValidate = (value) => {
+    // validate month
     value = MONTHS[value.toLowerCase()];
     return value !== undefined && value !== "" && value >= 0 
     && value <= 11;
@@ -8,7 +9,15 @@ const monthValidate = (value) => {
 
 const yearValidate = (value) => {
     // validate year
+    const n = Number(value);
+    return year !== "" && !isNaN(n) && n >= 1970;
+
 }
 
+const createCalendar = (monthValidate,yearValidate) => {
+    if(monthValidate && yearValidate){
+        console.log("Hola");
+    }
+}
 
 export { monthValidate, yearValidate };
