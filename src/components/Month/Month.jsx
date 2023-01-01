d7f68346d4c78324425c78683c486568f5983e4f
luis <luis.deharoparames@emeal.nttdata.com>
2023-01-01 19:36:52 +0100

    Changes

--- src/components/Month/Month.jsx
+++ src/components/Month/Month.jsx
@@ -2,6 +2,5 @@ import "../../styles/App.css";
 export const Month = ({ year, month }) => {
   // const intl = new Intl.DateTimeFormat("es", { month: "long" });
   // const monthName = intl.format(new Date(year, month));
-  console.log("dsd")
   return <>{month}</>;
 };
