b95fc6ea54ed1c83ee65192e5b3cc5c345b3f849
luis <luis.deharoparames@emeal.nttdata.com>
2023-01-02 11:51:57 +0100

    Dias de la semana

--- src/components/MonthWeekDays/MonthWeekDays.jsx
+++ src/components/MonthWeekDays/MonthWeekDays.jsx
@@ -1,6 +1,7 @@
 import "../../data/LOCALE";
 import { MonthDays } from "../MonthDays/MonthDays";
-export const MonthWeekDays = (locale, { year, month }) => {
+export const MonthWeekDays = (locale, {year, month}) => {
+  console.log("dias de la semana" + year,month);
   const intl = new Intl.DateTimeFormat(locale, { weekday: "long" });
   const weekDays = [...new Array(7).keys()].map((weekDay) => weekDay);
   const daysMonth = [...new Array(12).keys()].map((days) => days);
@@ -11,8 +12,10 @@ export const MonthWeekDays = (locale, { year, month }) => {
           {intl.format(new Date(1973, 0, n + 1))}
         </div>
       ))}
-      {daysMonth.map((m) => (
-        <MonthDays year={year} month={m} />
+      {daysMonth.map((numMonth) => (
+        <div key={numMonth} className="">
+          <MonthDays year={year} month={numMonth} />
+        </div>
       ))}
     </>
   );
