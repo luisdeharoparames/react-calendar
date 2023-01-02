00e21e913c690868e89cac3921745d674ce3a05f
luis <luis.deharoparames@emeal.nttdata.com>
2023-01-02 11:52:42 +0100

    Dias de el Mes

--- src/components/MonthDays/MonthDays.jsx
+++ src/components/MonthDays/MonthDays.jsx
@@ -1,11 +1,4 @@
 import "../../styles/App.css";
-export const MonthDays = ({ month, year }) => {
-  year = 2022;
-  const dayMonth = new Date(year, month + 1, 0).getDate();
-  const startOn = new Date(year, month, 1).getDay();
-  const arrayDays = [];
-  for (let i = 0; i < dayMonth; i++) {
-    arrayDays.push(i + 1);
-  }
-  return <>{arrayDays}</>;
+export const MonthDays = ({year,numMonth}) => {
+  // console.log(year,numMonth);
 };
